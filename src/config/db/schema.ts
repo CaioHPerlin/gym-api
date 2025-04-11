bd9b8b6a8000148512e91615774418b88db543bf
CaioHPerlin <caiohperlin@gmail.com>
2025-04-10 21:50:52 -0400

    chore: set user schema up

--- src/config/db/schema.ts
+++ src/config/db/schema.ts
@@ -1,8 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp, boolean, text } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const user = pgTable("user", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	name: varchar({ length: 255 }).notNull(),
-	age: integer().notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
+	passwordHash: text().notNull(), // Store hashed passwords
+	age: integer(),
+	isActive: boolean().default(true).notNull(), // For soft deletes or account status
+	createdAt: timestamp().defaultNow().notNull(), // Automatically set on creation
+	updatedAt: timestamp().defaultNow().notNull(), // Automatically updated on modification
+	deletedAt: timestamp(), // For soft deletes
 });
