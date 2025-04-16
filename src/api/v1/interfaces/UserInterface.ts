c2e766b7f1713bcad79953465ee6ca83255e530a
CaioHPerlin <caiohperlin@gmail.com>
2025-04-16 15:29:13 -0400

    refactor: rename interfaces

--- src/api/v1/interfaces/UserInterface.ts
+++ src/api/v1/interfaces/UserInterface.ts
@@ -3,9 +3,6 @@ import { z } from "zod";
 
 export const userInputSchema = z.object({
 	name: z.string(),
-	cpf: z.string().regex(/^\d{11}$/, "CPF must have exactly 11 digits"),
-	rg: z.string().min(5).max(14),
-	oab: z.string().optional(),
 	email: z.string().email(),
 	password: z.string().min(6),
 	birthDate: z.string().date(),
--- src/api/v1/interfaces/index.ts
+++ src/api/v1/interfaces/index.ts
@@ -1,2 +1,2 @@
-export * from "./HealthStatus";
-export * from "./User";
+export * from "./HealthStatusInterface";
+export * from "./UserInterface";
