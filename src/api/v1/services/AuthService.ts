04fd3e175582e10beee1147848934fa187b93617
CaioHPerlin <caiohperlin@gmail.com>
2025-04-28 20:49:26 -0400

    feat: implement json web token signing

--- src/api/v1/services/AuthService.ts
+++ src/api/v1/services/AuthService.ts
@@ -1,16 +1,17 @@
 import { UserService } from "./UserService";
 import { PasswordService } from "./PasswordService";
 import { UnauthorizedError } from "@/errors";
-import { UserSelect } from "@/api/v1/interfaces";
 import { LoginInput } from "@/api/v1/interfaces/AuthInterface";
+import * as jwt from "jsonwebtoken";
+import env from "@/config/env";
 
 export class AuthService {
 	private readonly userService = new UserService();
 	private readonly passwordService = new PasswordService();
 
 	async login({ email, password }: LoginInput): Promise<{
-		user: UserSelect;
-		token: string;
+		accessToken: string;
+		refreshToken: string;
 	}> {
 		const user = await this.userService.findByEmail(email);
 		if (!user) throw new UnauthorizedError("Invalid credentials");
@@ -20,14 +21,23 @@ export class AuthService {
 			user.passwordHash
 		);
 		if (!isValidPassword) throw new UnauthorizedError("Invalid credentials");
-
 		if (!user.isActive) throw new UnauthorizedError("This account is currently disabled");
 
-		const token = this.generateMockToken(user.id); // TODO Replace with real logic
-		return { user, token };
-	}
+		// User exists and credentials are valid. Sign JWTs.
+		const payload = {
+			sub: user.id,
+			email: user.email,
+			name: user.name,
+		};
 
-	private generateMockToken(userId: number): string {
-		return `token-for-${userId}`;
+		const accessToken = jwt.sign(payload, env.ACCESS_TOKEN_SECRET, {
+			expiresIn: "30m",
+		});
+
+		const refreshToken = jwt.sign({ sub: payload.sub }, env.REFRESH_TOKEN_SECRET, {
+			expiresIn: "1d",
+		});
+
+		return { accessToken, refreshToken };
 	}
 }
